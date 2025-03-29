4eb56f446bea12980c513f5ffe3e70197dc688fb
gwangbu-desu <kwy1379@ajou.ac.kr>
2025-03-29 16:20:06 +0900

    Fix: Config Server 설정 로컬 테스트 할떄에는 spring.profiles.active설정을 native로 변경

--- src/main/java/org/optimagrowth/configserver/ConfigServerApplication.java
+++ src/main/java/org/optimagrowth/configserver/ConfigServerApplication.java
@@ -1,5 +1,6 @@
 package org.optimagrowth.configserver;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -7,8 +8,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
-
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
